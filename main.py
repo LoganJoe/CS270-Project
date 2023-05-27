0056fd1cbd828597d2dd7cb274bb211fa822ddc6
libn <libn@shanghaitech.edu.cn>
2023-05-27 11:01:03 +0800

    fix bugs in Motion Blur kernel

--- main.py
+++ main.py
@@ -54,13 +54,16 @@ def get_MotionBlur_kernel(size, a, b, T):
     :return: MotionBlur kernel in frequency domain
     """
     kernel = np.zeros((size, size)).astype(np.complex128)
-    for u in range(size):
-        for v in range(size):
+    for u in range(-size // 2, size // 2):
+        for v in range(-size // 2, size // 2):
             param = np.pi * (u * a + v * b)
             if param == 0:
-                kernel[u][v] = T * np.exp(-1.0j * param)
+                kernel[u + size // 2][v + size // 2] = T * np.exp(-1.0j * param)
             else:
-                kernel[u][v] = T / param * np.sin(param) * np.exp(-1.0j * param)
+                kernel[u + size // 2][v + size // 2] = T / param * np.sin(param) * np.exp(-1.0j * param)
+    img = np.abs(kernel)
+    img = (img - np.min(img)) / (np.max(img) - np.min(img))
+    cv2.imwrite("MB_kernel.jpg", img * 255)
     return kernel
 
 
@@ -80,8 +83,9 @@ def restore_Trump(img, R=None, K=150):
     return restored_img
 
 
-def restore_Biden(img, a=0.025, b=-0.025, T=50, K=20):
+def restore_Biden(img, a=0.0205, b=-96 / 57 * 0.0205, T=100, K=100):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Biden_fft = visualize_DFT(img_gray, display=True)
     MB_kernel = get_MotionBlur_kernel(img_gray.shape[0], a, b, T)
     restored_img = Wiener_filter(img, MB_kernel, K)
     return restored_img
@@ -102,7 +106,7 @@ def Wiener_filter(img, kernel, K):
     else:
         img_fft = DFT(img)
         res_fft = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
-        res = np.abs(np.fft.ifft2(np.fft.ifftshift(img_fft * res_fft)))
+        res = np.abs(IDFT(img_fft * res_fft))
         res = ((res - np.min(res)) / (np.max(res) - np.min(res)) * 255).astype(np.uint8)
         return res
 
@@ -115,6 +119,6 @@ if __name__ == "__main__":
     res_Biden = restore_Biden(Biden)
     cv2.imwrite('restored_Trump.jpg', res_Trump)
     cv2.imwrite('restored_Biden.jpg', res_Biden)
-    pyramids = Pyramids()
-    blend = pyramids.pyramid_blending(res_Trump, res_Biden, mask)
-    cv2.imwrite('Biden&Trump.jpg', pyramids.reconstruct(blend))
+    # pyramids = Pyramids()
+    # blend = pyramids.pyramid_blending(res_Trump, res_Biden, mask)
+    # cv2.imwrite('Biden&Trump.jpg', pyramids.reconstruct(blend))
